b3285b3e404723841e0c8db0c3029afce39febf7
Unknown <19Sebastian95@gmx.de>
2019-12-29 01:05:36 +0100

    renamed env var for ws port

--- brocker/src/index.ts
+++ brocker/src/index.ts
@@ -2,5 +2,5 @@ import {WebSocket} from "./classes/WebSocket"
 import { Brocker } from "./classes/Brocker";
 
 console.log("Starting Server...");
-const wsPort: number = Number(process.env.MUMBLE_WEB_KEY) || 8080;
+const wsPort: number = Number(process.env.MUMBLE_WEB_WS_PORT) || 8080;
 /*const socket =*/ new WebSocket(wsPort);
