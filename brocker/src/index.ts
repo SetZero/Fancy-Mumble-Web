c18dee1c11a30e4cbda1c3e57971d45e6a75cbab
Unknown <19Sebastian95@gmx.de>
2020-03-06 22:18:55 +0100

    add backend to build

--- brocker/src/index.ts
+++ brocker/src/index.ts
@@ -6,3 +6,4 @@ const webServerPort: number = Number(process.env.MUMBLE_WEB_WS_WEB_PORT) || 8080
 const wsPort: number = Number(process.env.MUMBLE_WEB_WS_PORT) || 8081;
 const helperPort: number = Number(process.env.MUMBLE_WEB_WS_HELPER_PORT) || 8082;
 /*const socket =*/ new WebSocketHelper(wsPort, helperPort, webServerPort);
+console.log("Started Server, listening on Port %d, %d, %d", webServerPort, wsPort, helperPort);
